aa3402969f9cb0a482850b62ee25a895d5bb5c26
vodennis <dvoburris@gmail.com>
2024-12-10 10:38:52 -0500

    Update service baseUrl

--- project3/src/services/recipe.ts
+++ project3/src/services/recipe.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const newAxios = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
 });
 
 export default {
