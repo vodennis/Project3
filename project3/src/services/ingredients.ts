e4eb1ec9a7d9e25fb5def00721ce81f8581af318
vodennis <dvoburris@gmail.com>
2024-12-10 11:19:34 -0500

    Update services

--- project3/src/services/ingredients.ts
+++ project3/src/services/ingredients.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const newAxios = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 export default {
